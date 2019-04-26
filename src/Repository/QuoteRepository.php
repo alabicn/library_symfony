a6805d38495a13151ea183bdff0d5563fe1898ba
unknown <alabicn@hotmail.com>
2019-04-26 16:25:54 +0200

    Page 'Edit book's details created'

--- src/Repository/QuoteRepository.php
+++ src/Repository/QuoteRepository.php
@@ -19,38 +19,10 @@ class QuoteRepository extends ServiceEntityRepository
         parent::__construct($registry, Quote::class);
     }
 
-    // /**
-    //  * @return Quote[] Returns an array of Quote objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('q.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Quote
-    {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
-
     public function findAllOrderedByName() {
         $qb = $this->createQueryBuilder('quote')
-                ->getQuery();
+                    ->orderBy('quote.author', 'ASC')
+                    ->getQuery();
 
         return $qb->execute();
     }
